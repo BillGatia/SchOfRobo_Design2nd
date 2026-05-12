2990d773f4a2bb6e7d44a07f529d3178e313df89
Bill <3482275682@qq.com>
2026-05-12 14:11:42 +0800

    吃完午饭又玩了一小时，也不知道调了啥

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/bill/ProjectDesign2/SchOfRobo_Design2nd/BlackLine01/BlackLine.cpp" "CMakeFiles/main.dir/BlackLine01/BlackLine.cpp.o" "gcc" "CMakeFiles/main.dir/BlackLine01/BlackLine.cpp.o.d"
+  "/home/bill/ProjectDesign2/SchOfRobo_Design2nd/Operation/Operation.cpp" "CMakeFiles/main.dir/Operation/Operation.cpp.o" "gcc" "CMakeFiles/main.dir/Operation/Operation.cpp.o.d"
+  "/home/bill/ProjectDesign2/SchOfRobo_Design2nd/TrafficLight02/TrafficLight02.cpp" "CMakeFiles/main.dir/TrafficLight02/TrafficLight02.cpp.o" "gcc" "CMakeFiles/main.dir/TrafficLight02/TrafficLight02.cpp.o.d"
+  "/home/bill/ProjectDesign2/SchOfRobo_Design2nd/main.cpp" "CMakeFiles/main.dir/main.cpp.o" "gcc" "CMakeFiles/main.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/BlackLine01/BlackLine.cpp.o"
+  "CMakeFiles/main.dir/BlackLine01/BlackLine.cpp.o.d"
+  "CMakeFiles/main.dir/Operation/Operation.cpp.o"
+  "CMakeFiles/main.dir/Operation/Operation.cpp.o.d"
+  "CMakeFiles/main.dir/TrafficLight02/TrafficLight02.cpp.o"
+  "CMakeFiles/main.dir/TrafficLight02/TrafficLight02.cpp.o.d"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
